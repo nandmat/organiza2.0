815ea3ee9d557ac03c861d31a9448d1e785d6a9b
nandmat <nandersonmatheusmelo@gmail.com>
2022-10-20 23:34:20 -0300

    adicionando javascript para função de relógio em tempo real e campo para limpar lista de tarefas

--- index.php
+++ index.php
@@ -8,9 +8,13 @@ if(!isset($_SESSION['tasks'])){
 
 if(isset($_GET['task_name'])){
     array_push($_SESSION['tasks'], $_GET['task_name']);
+    unset($_GET['task_name']);
 }
 
-var_dump($_SESSION['tasks'])
+if(isset($_GET['clear'])){
+    unset($_SESSION['tasks']);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -19,21 +23,29 @@ var_dump($_SESSION['tasks'])
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" 
 rel="stylesheet"> 
-    <title>Tarefas</title>
+    <title>Organiza 2.0</title>
 </head>
 <body>
     <main class="container">
         <header class="header">
             <h1>Organiza</h1>
         </header>
+        <div class="two_header">
+        <div id="nome"></div>
+            <section class="relogio">
+                <div class="display">
+                    00:00:00
+                </div>
+            </section>
+        </div>
         <section class="form">
             <form action="" method="get">
-                <label for="task_name">Tarefa</label>
+            <label for="task_name">Digite sua tarefa</label>
                 <input type="text" name="task_name" placeholder="Tarefa">
                 <button type="submit">Adicionar</button>
             </form>
@@ -41,16 +53,30 @@ rel="stylesheet">
         <section class="separador">
         </section>
         <section class="list-tasks">
-            <ul>
-                <li>Tarefa -> 1</li>
-                <li>Tarefa -> 2</li>
-                <li>Tarefa -> 3</li>
-            </ul>
+            <?php 
+            
+            if(isset($_SESSION['tasks'])){
+                echo '<ul>';
+                
+                foreach($_SESSION['tasks'] as $key => $task){
+                    echo "<li>$task</li>";
+                }
+
+                echo'</ul>';
+            }
+            
+            ?>
+
+            <form action="" method="get">
+                <input type="hidden" name="clear" value="clear">
+                <button type="submit">Limpar</button>
+            </form>
         </section>
         <footer class="footer">
             <p>by Nanderson Matheus Reis</p>
         </footer>
     </main>
 
+    <script src="./assets/js/time.js"></script>
 </body>
 </html>
